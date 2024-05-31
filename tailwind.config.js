9c2d1ed564ccbd596f0cb02d7895f597082767aa
AhlanAlghiff <ahlanalghiff@gmail.com>
2024-05-31 17:46:03 +0700

    Login page selesai

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/{html,js}"],
+  content: ["./src/**/*.{html,js,php}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        first: '#6CABDD',
+        second: '#1C2C5B',
+        third: '#00529F',
+        fourth: '#FEBE10',
+        fifth: '#01060f',
+        sixth: '#e5e5e5',
+      },
+      fontFamily: {
+        poppins: ["Poppins", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
